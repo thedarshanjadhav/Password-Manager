bc9bf87f346a27616406da4aef5c95b4c0066f30
Darshan Jadhav <darshanjadhav363@gmail.com>
2024-11-26 14:13:34 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <div className="bg-slate-800 text-white flex flex-col justify-center items-center  w-full">
+      <div className="logo font-bold text-white text-2xl">
+        <span className="text-green-500"> &lt;</span>
+
+        <span>Pass</span>
+        <span className="text-green-500">OP/&gt;</span>
+      </div>
+      <div className="flex justify-center items-center">
+        {" "}
+        The Password <img
+          className="w-7 mx-2"
+          src="icons/heart.png"
+          alt=""
+        />{" "}
+        Manager{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
